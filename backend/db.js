5ffacff382bff4eee35ad6510f372a7a64b569f9
Eidan <eidankhan.student.muet@gmail.com>
2021-11-18 13:00:19 +0500

    Conneted to remotely hosted mongodb

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+
+// Connect to local mongodb
+// const mongoURI = 'mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+
+// Connect to remotely hosted db 
+const mongoURI = "mongodb+srv://eidancodez:khan@remotedb.ywdty.mongodb.net/iNotebook"
 
 const connectToMongodb = () => {
     mongoose.connect(mongoURI, () => {
